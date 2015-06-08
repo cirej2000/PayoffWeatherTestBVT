273e4dec44a5774c1f66f729e8f5a39992cc526a
Eric Jones <cirej2013@gmail.com>
2015-06-07 21:21:58 -0700

    Brought more tests online.  However, there seems to be a problem with the Java objects coming through.  Need to investigate the parsing of the Java Object for error messages.  Also, expected 405 errors are coming back as 404s.  Also need to investigate.

--- src/test/java/com/payoff/weather/test/bvt/PayoffWeatherTestBVT.java
+++ src/test/java/com/payoff/weather/test/bvt/PayoffWeatherTestBVT.java
@@ -39,67 +39,124 @@ public class PayoffWeatherTestBVT {
     @DataProvider(name = "error-message-tests")
     public Object[][] errorTestsData() {
         return new Object[][]{
-                {"GET", 200, "", 1, "Location Not Found"},
-                {"GET", 200, "Validivostok,CA", 1, "Location Not Found"},
-                {"GET", 200, "-92129", 1, "Location Not Found"},
-                {"GET", 200, "Los Angeles", 1008, "Some parameters required by the method are missing"},
                 {"POST",405, "Los Angeles", 1002, "The requested method doesn't support this HTTP Method provided"},
                 {"PUT", 405, "Los Angeles", 1002, "The requested method doesn't support this HTTP Method provided"},
                 {"DELETE", 405, "Los Angeles", 1002, "The requested method doesn't support this HTTP Method provided"}
-
         };
     }
-  /*  @BeforeClass
-    public void setUp()
-    {
-        log = Logger.getLogger(PayoffWeatherTestBVT.class);
-        log.info("Starting test...");
-    }*/
-    @BeforeTest
-    public void setClient(){
-        log.info("Setting up REST client for checkValid Weather...");
-        serviceClient = new PayoffWeatherServiceClient();
-        log.info("Service client ready.  Starting test.");
+
+    @DataProvider(name = "bad-location-tests")
+    public Object[][] badLocationData() {
+        return new Object[][]{
+                {"GET", 200, "", 1, "Location Not Found"},
+                {"GET", 200, "Validivostok,CA", 1, "Location Not Found"},
+                {"GET", 200, "-92129", 1, "Location Not Found"}
+         };
     }
+
     @Test(dataProvider = "good-locations-tz")
     public void checkValidWeather(String method,int expectedStatus, String city, int timeZoneOffset, int numberOfDays){
        // String jsonResult = "";
+        log.info("Setting up REST client for checkValid Weather...");
+        serviceClient = new PayoffWeatherServiceClient(false);
+        log.info("Service client ready.  Starting test.");
         log.info("Starting weather check for location = "+city);
         System.out.println("Starting weather check for location = "+city);
 
-        if (method.toLowerCase().equals("get")){
-            serviceClient.get(city);
-            String jsonWeatherResult = serviceClient.getResponseBody();
-            Assert.assertEquals(serviceClient.getResponseStatusCode(),200);
-            Assert.assertEquals(ParsePayoffWeatherData.getResultSetSize(jsonWeatherResult),7, "For "+city+":  We expected to see 7 elements in the result set, but only, "+ParsePayoffWeatherData.getResultSetSize(jsonWeatherResult)+", were available.");
-            for (int x=1;x<=7;x++)
-            {
+        serviceClient.callWeatherService(method, city);
+        String jsonWeatherResult = serviceClient.getResponseBody();
+        Assert.assertEquals(serviceClient.getResponseStatusCode(),200);
+        Assert.assertEquals(ParsePayoffWeatherData.getResultSetSize(jsonWeatherResult),7, "For "+city+":  We expected to see 7 elements in the result set, but only, "+ParsePayoffWeatherData.getResultSetSize(jsonWeatherResult)+", were available.");
+        for (int x=1;x<=7;x++)
+        {
 
-                if (x==1)
-                {
-                    Assert.assertEquals(ParsePayoffWeatherData.getDayOfWeek(x,jsonWeatherResult).toLowerCase(),getThreeCharDayOfWeek(timeZoneOffset).toLowerCase(), "For "+city+":  We expected the first day of the week to be:  "+getThreeCharDayOfWeek(timeZoneOffset)+".  But we got:  "+ParsePayoffWeatherData.getDayOfWeek(x,jsonWeatherResult)+", instead.");
-                }
-                Assert.assertNotEquals(ParsePayoffWeatherData.getDailyHigh(x,jsonWeatherResult),"", "For "+city+":  We have empty data for the fahrenheit high temp!");
-                Assert.assertNotEquals(ParsePayoffWeatherData.getDailyLow(x, jsonWeatherResult),"", "For "+city+":  We have blank data for the fahrenheit low temp!");
-                Assert.assertNotEquals(ParsePayoffWeatherData.getDailyHighC(x,jsonWeatherResult),"", "For "+city+":  We have blank data for the celsius high temp!");
-                Assert.assertNotEquals(ParsePayoffWeatherData.getDailyLowC(x, jsonWeatherResult),"", "For "+city+":  We have blank data for the celsius low temp!");
+            if (x==1)
+            {
+                Assert.assertEquals(ParsePayoffWeatherData.getDayOfWeek(x,jsonWeatherResult).toLowerCase(),getThreeCharDayOfWeek(timeZoneOffset).toLowerCase(), "For "+city+":  We expected the first day of the week to be:  "+getThreeCharDayOfWeek(timeZoneOffset)+".  But we got:  "+ParsePayoffWeatherData.getDayOfWeek(x,jsonWeatherResult)+", instead.");
             }
+            Assert.assertNotEquals(ParsePayoffWeatherData.getDailyHigh(x,jsonWeatherResult),"", "For "+city+":  We have empty data for the fahrenheit high temp!");
+            Assert.assertNotEquals(ParsePayoffWeatherData.getDailyLow(x, jsonWeatherResult),"", "For "+city+":  We have blank data for the fahrenheit low temp!");
+            Assert.assertNotEquals(ParsePayoffWeatherData.getDailyHighC(x,jsonWeatherResult),"", "For "+city+":  We have blank data for the celsius high temp!");
+            Assert.assertNotEquals(ParsePayoffWeatherData.getDailyLowC(x, jsonWeatherResult),"", "For "+city+":  We have blank data for the celsius low temp!");
         }
-        //log.info("CheckValidWeather for location "+city+", has passed.");
+        log.info("Weather check for location = "+city+" Passed");
+        System.out.println("Weather check for location = "+city+" Passed");
     }
 
-   /* @Test(dataProvider = "error-message-tests")
-    public void checkErrorHandling(String method, int expectedStatus, String city, int errorCode, String errorMessage){
+    @Test(dataProvider = "bad-location-tests")
+    public void badLocationTests(String method, int expectedStatus, String city, int errorCode, String errorMessage){
+        System.out.println("Sending through bad locations and validating the correct error code/msg");
+        log.info("Bad location attempted:  " + city + ".");
+        log.info("Setting up REST client for badLocationTests...");
+        serviceClient = new PayoffWeatherServiceClient();
+        log.info("Service client ready.  Starting test.");
 
-    }*/
+        serviceClient.callWeatherService(method,city);
+
+        String jsonWeatherResult = serviceClient.getResponseBody();
+        int statusCode = serviceClient.getResponseStatusCode();
+
+        Assert.assertEquals(expectedStatus,statusCode, "For bad locations, we should have get a "+expectedStatus+" http status code.  Instead we got:  "+statusCode+".");
+        //shoulda just made this method return the object and let the test do it
+        int errCode = ParsePayoffWeatherData.getErrorMessageCode(jsonWeatherResult);
+        Assert.assertEquals(errorCode,errCode,"The error code returned from the service for a bad location should be:  "+errorCode+". We instead got:  "+errCode);
+        String errMessage = ParsePayoffWeatherData.getErrorMessage(jsonWeatherResult);
+        Assert.assertEquals(errorMessage,errMessage,"The error code returned from the service for a bad location should be:  "+errorMessage+". We instead got:  "+errMessage);
+
+        log.info("BadlocationTests passed for bad location = "+city);
+        System.out.println("BadlocationTests passed for bad location = "+city);
+    }
+    @Test(dataProvider = "error-message-tests")
+    public void checkRestMethodCompliance(String method, int expectedStatus, String city, int errorCode, String errorMessage){
+            System.out.println("Only GET should be used for this method, we'll use:  "+method+", for city:  "+city+".");
+            log.info("Only GET should be used for this method, we'll use:  "+method+", for city:  "+city+".");
+            log.info("Setting up REST client for checkRestMethodCompliance...");
+            serviceClient = new PayoffWeatherServiceClient();
+            log.info("Service client ready.  Starting test.");
+
+            serviceClient.callWeatherService(method,city);
+
+            String jsonWeatherResult = serviceClient.getResponseBody();
+            int statusCode = serviceClient.getResponseStatusCode();
+
+            Assert.assertEquals(expectedStatus,statusCode, "For http method:  "+method+ ", we should have gotten a "+expectedStatus+" http status code.  Instead we got:  "+statusCode+".");
+            //shoulda just made this method return the object and let the test do it
+            int errCode = ParsePayoffWeatherData.getErrorMessageCode(jsonWeatherResult);
+            Assert.assertEquals(errorCode,errCode,"The error code returned from the service for an incorrect method should be:  "+errorCode+". We instead got:  "+errCode);
+            String errMessage = ParsePayoffWeatherData.getErrorMessage(jsonWeatherResult);
+            Assert.assertEquals(errorMessage,errMessage,"The error code returned from the service for an incorrect method should be:  "+errorMessage+". We instead got:  "+errMessage);
+
+            log.info("CheckRestMethodCompliance passed for method = "+method);
+            System.out.println("CheckRestMethodCompliance passed for method = "+method);
+    }
+
+    @Test
+    public void brokenUrlTest(){
+        System.out.println("Testing with missing location query parameter, should get a 200 status but an error message.");
+        log.info("Testing with missing location query parameter, should get a 200 status but an error message.");
+        serviceClient = new PayoffWeatherServiceClient(true);
+        serviceClient.callWeatherService("get","paris");
+
+        String jsonWeatherResult = serviceClient.getResponseBody();
+        int statusCode = serviceClient.getResponseStatusCode();
+
+        Assert.assertEquals(200,statusCode, "A missing URL should have resulted in a 200 http status code.  Instead we got:  "+statusCode+".");
+        //shoulda just made this method return the object and let the test do it
+        int errCode = ParsePayoffWeatherData.getErrorMessageCode(jsonWeatherResult);
+        Assert.assertEquals(1008,errCode,"The error code returned from the service for an incorrect method should be:  1008"+" We instead got:  "+errCode);
+        String errMessage = ParsePayoffWeatherData.getErrorMessage(jsonWeatherResult);
+        Assert.assertEquals("Some parameters required by the method are missing",errCode,"The error code returned from the service for an incorrect method should be:  'Some parameters required by the method are missing'. We instead got:  "+errMessage);
+        log.info("BrokenUrlTest passed for missing location parameter.");
+        System.out.println("BrokenUrlTest passed for missing location parameter.");
+    }
 
     public String getThreeCharDayOfWeek(int offset)
     {
         TimeZone tz;
         if (offset < 0)
-            tz = TimeZone.getTimeZone("GMT-"+Math.abs(offset));
+            tz = TimeZone.getTimeZone("GMT-"+Integer.toString(Math.abs(offset)));
         else
-            tz = TimeZone.getTimeZone(("GMT+"+offset));
+            tz = TimeZone.getTimeZone(("GMT+"+Integer.toString(offset)));
         Calendar rightNow = Calendar.getInstance(tz);
         return new SimpleDateFormat("EEE").format(rightNow.getTime());
     }
