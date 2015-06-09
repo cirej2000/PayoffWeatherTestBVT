23c189319cde8bec5b51af00a6cb7a9ab4e2ec41
Eric Jones <cirej2013@gmail.com>
2015-06-09 04:50:56 -0700

    testing this push to gh-pages

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
   echo -e "Starting to update gh-pages\n"
 
   #copy data we're interested in to other place
@@ -7,7 +7,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #go to home and setup git
   cd $HOME
   git config --global user.email "cirej2013@gmail.com"
-  git config --global user.name "Cirej2000"
+  git config --global user.name "cirej2000"
 
   #using token clone gh-pages branch
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/cirej2000/PayoffWeatherTestBVT.git  gh-pages > /dev/null
@@ -18,7 +18,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #add, commit and push files
   git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git commit -m "Travis test results for $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
   echo -e "Done magic with coverage\n"
